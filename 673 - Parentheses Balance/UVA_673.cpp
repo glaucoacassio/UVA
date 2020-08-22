fdcda18230a03b0ba6f6ebfc6679c0c53c78d032
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-08-22 15:56:31 -0300

    Create UVA_673.cpp

---
+++ 673 - Parentheses Balance/UVA_673.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <cstring>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int n;
+    char linha[1000];
+    stack<char> pilha;
+
+    scanf("%d", &n);
+    getchar();
+    while (n--)
+    {
+        bool ok = true;
+        gets(linha);
+        for (int i = 0; i < strlen(linha); i++)
+        {
+            if (linha[i] == '(' || linha[i] == '[')
+                pilha.push(linha[i]);
+            else if (linha[i] == ')' || linha[i] == ']')
+            {
+                if (!pilha.empty() && linha[i] == ')' && pilha.top() == '(')
+                    pilha.pop();
+                else if (!pilha.empty() && linha[i] == ']' && pilha.top() == '[')
+                    pilha.pop();
+                else
+                {
+                    ok = false;
+                    break;
+                }
+            }
+        }
+        if (ok && pilha.empty()) printf("Yes\n");
+        else printf("No\n");
+
+        while (!pilha.empty())
+                pilha.pop();
+    }
+    return 0;
+}
