26d4c8e8e3073e91c752c9b661da6a763e1a160e
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-08-28 00:54:45 -0300

    Update Longest Palindrome.cpp

--- 11151 - Longest Palindrome/Longest Palindrome.cpp
+++ 11151 - Longest Palindrome/Longest Palindrome.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
 #include <string>
+#include <cstring>
+#include <cstdio>
+#include <stack>
+#include <string.h>
+#include <queue>
+#include <iomanip>
+#include <math.h>
+#include <utility>
+#include <sstream>
+#include <cstdlib>
+#include <fstream>
+#include <climits>
 using namespace std;
 
 string s;
 int len, dp[1005][1005];
 
-int sol(int l, int r)
+int longestPalin(int l, int r)
 {
-	if (dp[l][r] != -1) return dp[l][r];
-	if (l == r) return dp[l][r] = 1;
-	else if (r == l + 1)
+	if (l == r)
+		return 1;
+	if (l + 1 == r)
 	{
-		if (s[l] == s[r]) return dp[l][r] = 2;
-		else return dp[l][r] = 1;
+		if (s[l] == s[r])
+			return 2;
+		else
+			return 1;
 	}
-	if (s[l] == s[r]) return 2 + sol(l + 1, r - 1);
-	else return dp[l][r] = max(sol(l + 1, r), sol(l, r - 1));
+	if (dp[l][r] != -1)
+		return dp[l][r];
+
+	if (s[l] == s[r])
+		return dp[l][r] = 2 + longestPalin(l + 1, r - 1);
+	else
+		return dp[l][r] = max(longestPalin(l, r - 1), longestPalin(l + 1, r));
 }
 
 int main()
@@ -29,10 +53,8 @@ int main()
 	{
 		getline(cin, s);
 		len = s.length();
-		for (int i = 0; i < 1005; i++)
-		for (int j = 0; j < 1005; j++)
-			dp[i][j] = -1;
-		cout << sol(0, len - 1) << endl;
+		memset(dp, -1, sizeof(dp));
+		cout << longestPalin(0, len - 1) << endl;
 	}
 	return 0;
 }
