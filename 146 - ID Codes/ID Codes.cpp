47dad6a3b03c367fef8cdcc2d78210132eb7b562
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-09-01 10:22:37 -0300

    Create ID Codes.cpp

---
+++ 146 - ID Codes/ID Codes.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <stack>
+#include <string.h>
+#include <queue>
+#include <iomanip>
+#include <math.h>
+#include <utility>
+#include <sstream>
+#include <cstdlib>
+#include <fstream>
+#include <climits>
+#include <clocale>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define SQ(a) (a) * (a)
+//compara se numero flutuantes sao iguais if(abs(a-b) < 1e-9)   a e b sao iguais...
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long int ll;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	string s;
+	
+	while(cin >> s, s != "#")
+	{	
+      if(next_permutation(s.begin(), s.end())) cout << s << endl;
+      else cout << "No Successor" << endl;
+	}
+	return 0;
+}
